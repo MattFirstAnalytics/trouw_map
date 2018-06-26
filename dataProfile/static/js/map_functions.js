a090c0cba970a1404bad42ab388b68dd0bc59427
Matt <matthewyancey@gmail.com>
2018-06-26 00:10:47 -0400

    map selectio nedits

--- dataProfile/static/js/map_functions.js
+++ dataProfile/static/js/map_functions.js
@@ -1,167 +1,184 @@
 var map = L.map('map').setView([37.8, -96], 4);
-var selected_region = '';
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.light'
-}).addTo(map);
-
-
-// control that shows state info on hover
-var info = L.control();
-
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info');
-    this.update();
-    return this._div;
-};
-
-info.update = function (props) {
-    this._div.innerHTML = (props ? '<h4>Sales by BEA</h4><b>BEA Name: ' + props.name + '<br/><b>Volume: ' + props.density + ' MT':
-        '<h4>Sales by BEA</h4><b>BEA Name:   </br><b>Volume:');
-};
-
-info.addTo(map);
-
-
-mv = 0;
-lv = 100000;
-for (var i = 0; i < statesData["features"].length; i++) {
-    if (statesData["features"][i]["properties"]["density"] > mv) {
-        mv = statesData["features"][i]["properties"]["density"];
-    };
-    if (statesData["features"][i]["properties"]["density"] != '' && statesData["features"][i]["properties"]["density"] < lv) {
-        lv = statesData["features"][i]["properties"]["density"];
-    };
-
-};
-
-
-
-// color_scale = d3.scaleLog()
-//     .domain([lv, mv])
-//     .range(["#055faa", "#c1182b"]);
-
-color_scale = d3.scaleLinear()
-    .domain([lv, mv])
-    .range(["#055faa", "#c1182b"]);
-
-
-function style(feature) {
-    return {
-        weight: 1,
-        opacity: 0.5,
-        color: color_scale(feature.properties.density),
-        // dashArray: '3',
-        fillOpacity: 0.5,
-        fillColor: color_scale(feature.properties.density)
-    };
-}
-
-
-function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 1,
-        // color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-    }
-
-    info.update(layer.feature.properties);
-}
-
-var geojson;
-
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
-}
-
-function clickEvent(e) {
-    // map.fitBounds(e.target.getBounds());
-    selected_region = e.target.feature.properties.name;
-}
-
-function update_base(e) {
-
-}
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: clickEvent
-    });
-}
-
-geojson = L.geoJson(statesData, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
-
-map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
-
 
 var legend = L.control({
     position: 'bottomright'
 });
 
-legend.onAdd = function (map) {
+var info = L.control();
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [mv, lv + 3 * (mv - lv)/5, lv + 2 * (mv - lv)/5, lv + (mv - lv)/5,  lv],
-        labels = [],
-        from, to;
+function run_map(map_data){
 
-    for (var i = 0; i < grades.length; i++) {
-        from = parseInt(grades[i]);
-        to = grades[i + 1];
+    // clears the map of anything
+    map.eachLayer(function (layer) {
+        map.removeLayer(layer);
+    });
 
-        labels.push(
-            '<i style="background:' + color_scale(from + 1) + '"></i> ' + from);
-
-        // labels.push(
-        //     '<i style="background:' + color_scale(from + 1) + '"></i> ' +
-        //     from + (to ? '&ndash;' + to : '+'));
-    }
-
-    div.innerHTML = labels.join('<br>');
-    return div;
-};
-
-legend.addTo(map);
+    legend.remove();
+    info.remove();    
 
 
-// function for toggeling the areas on and off
-function flip(what_button){
-    console.log(what_button);
-    var position = document.getElementById(what_button).checked;
-    console.log(position);
-    if (position) {
-        var opacity_level = 0.5;
-    } else {
-        var opacity_level = 0.0;
+    var selected_region = '';
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.light'
+    }).addTo(map);
+
+
+    // control that shows state info on hover
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info');
+        this.update();
+        return this._div;
     };
 
-    map.eachLayer(function(layer) {
-        try {
-            if (layer.feature.properties.type == what_button){
-                layer.setStyle({fillOpacity: opacity_level});
-                if (opacity_level == 0.5){
-                    layer.setStyle({weight: opacity_level + 0.5});
-                } else {
-                    layer.setStyle({weight: opacity_level});
-                }
-            };
-        } catch (err) {}
-    });
+    info.update = function (props) {
+        this._div.innerHTML = (props ? '<h4>Sales by BEA</h4><b>BEA Name: ' + props.name + '<br/><b>Volume: ' + props.density + ' MT':
+            '<h4>Sales by BEA</h4><b>BEA Name:   </br><b>Volume:');
+    };
+
+    info.addTo(map);
+
+
+    mv = 0;
+    lv = 100000;
+    for (var i = 0; i < map_data["features"].length; i++) {
+        if (map_data["features"][i]["properties"]["density"] > mv) {
+            mv = map_data["features"][i]["properties"]["density"];
+        };
+        if (map_data["features"][i]["properties"]["density"] != '' && map_data["features"][i]["properties"]["density"] < lv) {
+            lv = map_data["features"][i]["properties"]["density"];
+        };
+
+    };
+
+
+
+    color_scale = d3.scaleLog()
+        .domain([lv, mv])
+        .range(["#055faa", "#c1182b"]);
+
+    // color_scale = d3.scaleLinear()
+    //     .domain([lv, mv])
+    //     .range(["#055faa", "#c1182b"]);
+
+    function density_check(density){
+        if(density == 0){
+            return(0);
+        } else{
+            return(0.5);
+        };
+    };
+
+    function style(feature) {
+        return {
+            weight: 1,
+            opacity: density_check(feature.properties.density),
+            color: color_scale(feature.properties.density),
+            // dashArray: '3',
+            fillOpacity: density_check(feature.properties.density),
+            fillColor: color_scale(feature.properties.density)
+        };
+    }
+
+
+    function highlightFeature(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 1,
+            // color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+
+        info.update(layer.feature.properties);
+    }
+
+    var geojson;
+
+    function resetHighlight(e) {
+        geojson.resetStyle(e.target);
+        info.update();
+    }
+
+    function clickEvent(e) {
+        // map.fitBounds(e.target.getBounds());
+        selected_region = e.target.feature.properties.name;
+    }
+
+    function update_base(e) {
+
+    }
+
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+            click: clickEvent
+        });
+    }
+
+    geojson = L.geoJson(map_data, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
+
+    map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [mv, lv + 3 * (mv - lv)/5, lv + 2 * (mv - lv)/5, lv + (mv - lv)/5,  lv],
+            labels = [],
+            from, to;
+
+        for (var i = 0; i < grades.length; i++) {
+            from = parseInt(grades[i]);
+            to = grades[i + 1];
+
+            labels.push(
+                '<i style="background:' + color_scale(from + 1) + '"></i> ' + from);
+
+            // labels.push(
+            //     '<i style="background:' + color_scale(from + 1) + '"></i> ' +
+            //     from + (to ? '&ndash;' + to : '+'));
+        }
+
+        div.innerHTML = labels.join('<br>');
+        return div;
+    };
+
+    legend.addTo(map);
+
 };
+
+    // function for toggeling the areas on and off
+    function flip(what_button){
+        var position = document.getElementById(what_button).checked;
+        if (position) {
+            var opacity_level = 0.5;
+        } else {
+            var opacity_level = 0.0;
+        };
+
+        map.eachLayer(function(layer) {
+            try {
+                if (layer.feature.properties.type == what_button){
+                    layer.setStyle({fillOpacity: opacity_level});
+                    if (opacity_level == 0.5){
+                        layer.setStyle({weight: opacity_level + 0.5});
+                    } else {
+                        layer.setStyle({weight: opacity_level});
+                    }
+                };
+            } catch (err) {}
+        });
+    };
